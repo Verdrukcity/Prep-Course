ac21d81a9dad842a364ed77f0e469a53be325962
Mota <motavercity@gmail,comm>
2022-07-21 18:36:10 -0500

    2modificacion

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -149,25 +149,30 @@ function elevarAlCubo(num) {
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-  
+  var elevado = Math.pow(num, exponent)
+  {return elevado;}
 }
 
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-  
+  var redondeado = Math.round (num)
+  {return redondeado;}
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
+  var redondeado = Math.ceil (num)
+  {return redondeado;}
   
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  
+  var random = Math.random()
+  {return random}
 }
 
 function esPositivo(numero) {
@@ -175,6 +180,12 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
+  if (numero > 0)
+  {return "Es positivo";}
+  else if (numero < 0)
+  {return "Es negativo";}
+  else (numero === 0)
+  {return false;}
   
 }
 
